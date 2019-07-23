a1971a9c25083bf15ab3ac4776a17a5ec408b85c
HUSTERGS <1148690954@qq.com>
2019-07-23 16:06:09 +0800

    mutipul file support

--- Backend/server.py
+++ Backend/server.py
@@ -4,5 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', jsonname = './graph_try_all.json')
 
+@app.route('/<jsonname>')
+def changeJson(jsonname):
+    return render_template('index.html', jsonname = jsonname)
